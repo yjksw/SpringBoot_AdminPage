12bc241eec294539c3af7b1d92a0a02047fcca9a
yunjung <cairofaith@gmail.com>
2020-10-17 16:46:04 +0900

    Front-end added

---
+++ src/main/java/com/example/study/model/network/response/UserOrderInfoApiResponse.java
@@ -0,0 +1,18 @@
+package com.example.study.model.network.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserOrderInfoApiResponse {
+
+    private UserApiResponse userApiResponse;
+
+}
